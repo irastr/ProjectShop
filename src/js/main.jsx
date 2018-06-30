f5d6de494a7d05e637653e32d5b010da9ea5acad
Constantine <kostyairvine@gmail.com>
2018-06-30 20:39:40 +0300

    adaptive upper-header

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  <h1></h1>,
-  document.getElementById('maincontent')
+let Counter = React.createClass({
+  incrementCount: function(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  },
+  decrementCount: function(){
+    this.setState({
+      count: this.state.count - 1
+    });
+  },
+  getInitialState: function(){
+     return {
+       count: 0
+     }
+  },
+  render(){
+    return (
+      <div className="counter">
+        <h1 className='count'>{this.state.count}</h1>
+        <button className="btn" onClick={this.decrementCount}>-</button>
+        <button className="btn" onClick={this.incrementCount}>+</button>
+      </div>
+    );
+  }
+});
+
+React.render(
+<Counter />,
+  document.getElementById('app')
 );
-  
